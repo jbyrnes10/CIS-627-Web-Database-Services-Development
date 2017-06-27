c4bab8fb94dc8fad6108b7705b2fa9a00c05551c
jbyrnes <johnbyrnes10@gmail.com>
2017-06-26 20:18:21 -0400

    week5

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.conf.urls import include
 from rango import views
 from registration.backends.simple.views import RegistrationView
+from django.contrib.auth.views import password_change, password_change_done
 
 class MyRegistrationView(RegistrationView):
     def get_success_url(self, user):
@@ -16,4 +17,10 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+    url(r'^accounts/password/change/$', password_change,
+        {'template_name': 'registration/password_change_form.html'},
+        name='password_change'),
+    url(r'^accounts/password/change/done/$', password_change_done,
+        {'template_name': 'registration/password_change_done.html'},
+        name='password_change_done'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
